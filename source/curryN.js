a41f48cd956867c5b3ef09f7569b293207bea9f2
brave xue <brave.xue@hexagon.com>
2020-01-07 09:57:00 +0800

    add some comments

--- source/curryN.js
+++ source/curryN.js
@@ -8,6 +8,7 @@ var curryN = _curry2(function curryN(length, fn) {
     return _curry1(fn);
   }
   // why not use _curryN(length, [], fn) dircetly
+  // if you don't use _arity to wrap the function, you will lose the length for curried function
   return _arity(length, _curryN(length, [], fn));
 });
 
